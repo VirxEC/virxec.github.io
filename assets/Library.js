6af89383664f0530dd754bff82014d6239bc5496
Virx <ericmveilleux@gmail.com>
2020-02-12 14:37:14 +0000

    Changed word

--- assets/Library.js
+++ assets/Library.js
@@ -5,7 +5,7 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License
  * 
- * This is the NodeJS/ModuleJS release of https://github.com/VirxEC/CalcPlus and https://www.virxcase.ga
+ * This is the Javascript release of https://github.com/VirxEC/CalcPlus and https://www.virxcase.ga
  */
 var powermode = false, // Feel free to change this, or use togglePowerMode();
   checks = true, // Feel free to change this, or use toggleAntiCheck();
