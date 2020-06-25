1150cb19822625c9d03cf1b4dbc02687d62b049e
Virx <ericmveilleux@gmail.com>
2020-06-25 17:23:27 -0400

    Update sitemap

--- main/sitemap.py
+++ main/sitemap.py
@@ -3,6 +3,6 @@ from django.shortcuts import reverse
 
 class StaticViewSitemap(Sitemap):
     def items(self):
-        return ['main_home']
+        return ['main_home', 'calcplus_preview', 'calcplus_source', 'options', 'virxeb']
     def location(self, item):
         return reverse(item)
