ba9d9d7e5e491246230050ab16e3c2bb45b344a4
Virx <ericmveilleux@gmail.com>
2020-06-25 17:15:10 -0400

    Fix deployment (6)

--- virxcase/wsgi.py
+++ virxcase/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'virxcase.settings')
 
 application = get_wsgi_application()
+
+os.system("python manage.py runserver")
