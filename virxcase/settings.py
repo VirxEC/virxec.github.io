7a666db9128260c0e036aaa9008281fea8c5f789
Virx <virx@virxcase.dev>
2020-07-04 11:55:03 -0400

    Config stuff

--- virxcase/settings.py
+++ virxcase/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'main', 'static')
 STATIC_URL = '/static/'
 
 PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'main', 'static', 'js', 'service-worker.js')
